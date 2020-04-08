f5d4336d051af978bd6b2cb8a4a2ca81a5c98d36
Bartosz Jaakubowski <bjakubov@gmail.com>
2020-04-08 09:27:30 +0200

    feat: Add all quotes in QuotesScreen

--- screens/QuotesScreen.js
+++ screens/QuotesScreen.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import {ScrollView, StyleSheet, Text} from "react-native";
+import {booksMock} from "../assets/mocks/books";
+import Quote from "../components/Quote";
 
 export default function QuotesScreen() {
 	return (
@@ -7,7 +9,16 @@ export default function QuotesScreen() {
 			style={styles.container}
 			contentContainerStyle={styles.contentContainer}
 		>
-			<Text>Quotes screen works</Text>
+			{booksMock.map((book) =>
+				book.quotes.map((quote) => (
+					<Quote
+						key={quote.id}
+						quote={quote}
+						author={book.author}
+						title={book.name}
+					/>
+				))
+			)}
 		</ScrollView>
 	);
 }
