6cd68d5b938cc8c44ca0bf7efaeaf347bfbd29eb
Bartosz Jaakubowski <bjakubov@gmail.com>
2020-04-10 11:09:28 +0200

    feat: Get quotes from store in BookList component

--- components/BookList.js
+++ components/BookList.js
@@ -1,20 +1,21 @@
 import * as React from "react";
 import {ScrollView, StyleSheet} from "react-native";
-import {booksMock} from "../assets/mocks/books";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 import BookListItem from "./BookListItem";
 
-export default function BookList() {
+export const BookList = ({quotes}) => {
 	return (
 		<ScrollView
 			style={styles.container}
 			contentContainerStyle={styles.contentContainer}
 		>
-			{booksMock.map((book) => (
+			{quotes.map((book) => (
 				<BookListItem key={book.id} {...book} />
 			))}
 		</ScrollView>
 	);
-}
+};
 
 const styles = StyleSheet.create({
 	contentContainer: {
@@ -22,3 +23,31 @@ const styles = StyleSheet.create({
 		paddingVertical: 15,
 	},
 });
+
+BookList.propTypes = {
+	quotes: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.string,
+			name: PropTypes.string,
+			author: PropTypes.string,
+			cover: PropTypes.string,
+			startedReading: PropTypes.number,
+			endedReading: PropTypes.number,
+			quotes: PropTypes.arrayOf(
+				PropTypes.shape({
+					id: PropTypes.string,
+					categories: PropTypes.arrayOf(PropTypes.string),
+					quote: PropTypes.string,
+				})
+			),
+		})
+	),
+};
+
+const mapStateToProps = (state) => {
+	return {
+		quotes: state.quoteReducer.quotes,
+	};
+};
+
+export default connect(mapStateToProps)(BookList);
