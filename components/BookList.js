184bdf91d6a8a65b0c757927ab0d19df88b82e2d
Bartosz Jaakubowski <bjakubov@gmail.com>
2020-04-21 14:17:53 +0200

    feat: Fetch books collection filtered by userId in BookList component

--- components/BookList.js
+++ components/BookList.js
@@ -1,18 +1,41 @@
-import * as React from "react";
-import {ScrollView, StyleSheet} from "react-native";
+import React, {useState, useEffect} from "react";
+import {ScrollView, StyleSheet, ActivityIndicator} from "react-native";
 import {connect} from "react-redux";
 import BookListItem from "./BookListItem";
 import {BooksPropTypes} from "../constants/PropTypes";
+import {firestore} from "../constants/Firebase";
+import {addBook} from "../store/actions/quote";
+import {Text} from "native-base";
 
-const BookList = ({books}) => {
+const BookList = ({uid, books, addBook}) => {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+
+		firestore
+			.collection("books")
+			.where("userId", "==", uid)
+			.get()
+			.then((books) =>
+				books.docs.forEach((book) => addBook({id: book.id, ...book.data()}))
+			)
+			.catch()
+			.finally(() => setLoading(false));
+	}, []);
+
 	return (
 		<ScrollView
 			style={styles.container}
 			contentContainerStyle={styles.contentContainer}
 		>
-			{books.map((book) => (
-				<BookListItem key={book.id} {...book} />
-			))}
+			{loading ? (
+				<ActivityIndicator size="large" />
+			) : books.length ? (
+				books.map((book) => <BookListItem key={book.id} {...book} />)
+			) : (
+				<Text>No books found</Text>
+			)}
 		</ScrollView>
 	);
 };
@@ -28,6 +51,7 @@ BookList.propTypes = BooksPropTypes;
 
 const mapStateToProps = (state) => ({
 	books: state.quoteReducer.books,
+	uid: state.authReducer.user.uid,
 });
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, {addBook})(BookList);
