6e642ac886cc204e7ccbafe0e38b971752f82caa
Bartosz Jaakubowski <bjakubov@gmail.com>
2020-04-21 14:16:35 +0200

    feat: Add creating and getting user in firestore

--- constants/Firebase.js
+++ constants/Firebase.js
@@ -23,6 +23,42 @@ export const signIn = (email, password) =>
 export const createUser = (email, password) =>
 	firebase.auth().createUserWithEmailAndPassword(email, password);
 
+export const createUserProfileDocument = async (user, additionalData) => {
+	if (!user) return;
+
+	const userRef = firestore.doc(`/users/${user.uid}`);
+	const snapshot = await userRef.get();
+
+	if (!snapshot.exists) {
+		const {displayName, email} = user;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.error("Error while creating user", error);
+		}
+	}
+
+	return getUserDocument(user.uid);
+};
+
+export const getUserDocument = async (uid) => {
+	if (!uid) return null;
+
+	try {
+		const userDocument = await firestore.collection("users").doc(uid).get();
+
+		return {uid, ...userDocument.data()};
+	} catch (error) {
+		console.error("Error fetching user", error.message);
+	}
+};
+
 export const signOut = firebase.auth().signOut();
 
 export default firebase;
