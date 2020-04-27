0bdc06a47af6e1889e58c39223522a11a08df7ac
Bartosz Jaakubowski <bjakubov@gmail.com>
2020-04-27 10:20:09 +0200

    feat: Add update/delete quote in quotes redux store

--- constants/ActionTypes.js
+++ constants/ActionTypes.js
@@ -1,8 +1,11 @@
 export const ADD_BOOK = "ADD_BOOK";
-export const ADD_QUOTE_TO_BOOK = "ADD_QUOTE_TO_BOOK";
 export const UPDATE_BOOK_DETAILS = "UPDATE_BOOK_DETAILS";
 export const DELETE_BOOK = "DELETE_BOOK";
 export const CLEAR_BOOKS = "CLEAR_BOOKS";
+
+export const ADD_QUOTE_TO_BOOK = "ADD_QUOTE_TO_BOOK";
+export const UPDATE_QUOTE = "UPDATE_QUOTE";
+export const DELETE_QUOTE = "DELETE_QUOTE";
 
 export const LOG_IN_USER = "LOG_IN_USER";
 export const LOG_OUT_USER = "LOG_OUT_USER";
